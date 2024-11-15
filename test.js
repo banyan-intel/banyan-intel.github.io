8991ab9953e64ee35ca58b84c32d8145fb423ea9
Arun Babu <arun.hbni@gmail.com>
2024-11-15 15:50:09 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 "use strict";
 
-import * as process from "node:process";                                         
-                                                                                 
+import * as process from "node:process";     
+import * as version from "./version.js";                   
+
 const name = process.argv[2] ?? "";                                              
                                                                                  
-console.log("Hello world!",name); 
+console.log(version,"Hello world!",name); 
